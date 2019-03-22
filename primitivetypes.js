da6b7b67bfd79682e250fc762ff2b8dc5c543107
kyildirim07 <kyildirim07@outlook.com>
2019-03-22 22:53:48 +0300

    referenceTypes.js added

--- primitivetypes.js
+++ primitivetypes.js
@@ -1,1 +1,21 @@
-// JavaScript source code
+// Primitive Types
+
+//String
+const name = "Kenan";
+
+//Number
+const age = 45;
+
+//Boolean
+const hasKids = true;
+
+//undefined
+let test;
+
+//Null
+const car = null;
+
+//Symbol
+const sym = Symbol();
+
+
---
+++ referenceTypes.js
@@ -0,0 +1,14 @@
+// Array
+
+const hobbies = ["movies", "music"];
+
+//Object Literal
+
+const address =
+{
+    city: "antalya",
+    state:"MA"
+}
+
+//Date
+var date = new Date();
